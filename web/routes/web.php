bc945802e2e943542d69910f392bfbad2d7553e7
John Freeman <prsjohnny@gmail.com>
2023-12-09 21:59:35 -0500

    basic auth

--- web/routes/web.php
+++ web/routes/web.php
@@ -3,32 +3,24 @@
 use App\Http\Controllers;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-Route::redirect('/', 'pipelines')->name('home');
+Route::middleware('auth.basic')->group(function () {
+    Route::redirect('/', 'pipelines')->name('home');
 
-Route::prefix('pipelines')->group(function () {
-    Route::get('/', [Controllers\PipelineController::class, 'index'])->name('pipelines.index');
-    Route::get('create', [Controllers\PipelineController::class, 'create'])->name('pipelines.create');
-    Route::post('store', [Controllers\PipelineController::class, 'store'])->name('pipelines.store');
+    Route::prefix('pipelines')->group(function () {
+        Route::get('/', [Controllers\PipelineController::class, 'index'])->name('pipelines.index');
+        Route::get('create', [Controllers\PipelineController::class, 'create'])->name('pipelines.create');
+        Route::post('store', [Controllers\PipelineController::class, 'store'])->name('pipelines.store');
 
-    Route::prefix('{pipeline}')->group(function () {
-        Route::get('/', [Controllers\PipelineController::class, 'show'])->name('pipelines.show');
-        Route::get('edit', [Controllers\PipelineController::class, 'edit'])->name('pipelines.edit');
-        Route::post('update', [Controllers\PipelineController::class, 'update'])->name('pipelines.update');
-        Route::post('run', [Controllers\PipelineController::class, 'run'])->name('pipelines.run');
+        Route::prefix('{pipeline}')->group(function () {
+            Route::get('/', [Controllers\PipelineController::class, 'show'])->name('pipelines.show');
+            Route::get('edit', [Controllers\PipelineController::class, 'edit'])->name('pipelines.edit');
+            Route::post('update', [Controllers\PipelineController::class, 'update'])->name('pipelines.update');
+            Route::post('run', [Controllers\PipelineController::class, 'run'])->name('pipelines.run');
 
-        Route::prefix('steps')->group(function () {
-            Route::prefix('{step}')->scopeBindings()->group(function () {
-                Route::post('run', [Controllers\PipelineController::class, 'runStep'])->name('pipelines.steps.run');
+            Route::prefix('steps')->group(function () {
+                Route::prefix('{step}')->scopeBindings()->group(function () {
+                    Route::post('run', [Controllers\PipelineController::class, 'runStep'])->name('pipelines.steps.run');
+                });
             });
         });
     });
