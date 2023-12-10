fcb19de4dc8595bdcbd7ff174669923bd31b9e0a
John Freeman <prsjohnny@gmail.com>
2023-12-10 00:36:47 -0500

    missing import

--- web/app/Webhooks/GithubSignatureValidator.php
+++ web/app/Webhooks/GithubSignatureValidator.php
@@ -4,6 +4,7 @@ namespace App\Webhooks;
 
 use Illuminate\Http\Request;
 use Spatie\WebhookClient\Exceptions\InvalidConfig;
+use Spatie\WebhookClient\SignatureValidator\SignatureValidator;
 use Spatie\WebhookClient\WebhookConfig;
 
 class GithubSignatureValidator implements SignatureValidator
