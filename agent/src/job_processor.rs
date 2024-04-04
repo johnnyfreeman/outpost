3c37c6ab4b8e7156122e201612580fc24a2882ac
John Freeman <prsjohnny@gmail.com>
2024-04-03 22:14:37 -0400

    hopefully, this solves the issue where certain commands aren't able to run

--- agent/src/job_processor.rs
+++ agent/src/job_processor.rs
@@ -16,68 +16,84 @@ impl JobProcessor {
         Self { queue, progress }
     }
 
-    pub async fn handle(&self, job: PipelineJob) -> Result<()> {
-        let mut job = Box::new(job);
-
-        self.progress.start(*job.clone()).await?;
+    pub async fn handle(&self, mut job: PipelineJob) -> Result<()> {
+        self.progress.start(job.clone()).await?;
 
         job.started_at = Some(Utc::now());
 
-        let mut child = tokio::process::Command::new("sh")
-            .arg("-c")
-            .arg(&job.step.script.clone())
-            .stdout(Stdio::piped())
-            .stderr(Stdio::piped())
-            .spawn()?;
+        for line in job.step.script.clone().lines() {
+            if let Some((command, args)) =
+                line.split_whitespace().collect::<Vec<&str>>().split_first()
+            {
+                let output = tokio::process::Command::new(command)
+                    .args(args)
+                    .output()
+                    .await?;
 
-        if let Some(stdout) = child.stdout.take() {
-            let mut lines = BufReader::new(stdout).lines();
-            while let Some(line) = lines.next_line().await? {
-                job.append_output(&line);
+                job.append_output(&String::from_utf8(output.stdout)?);
+                job.append_output(&String::from_utf8(output.stderr)?);
 
-                if self.queue.update(*job.clone()).await.is_err() {
+                match output.status.code() {
+                    Some(code) => match code {
+                        0 => {
+                            job.exit_code = output.status.code();
+
+                            self.progress.success(job.clone()).await?;
+                        }
+                        code => {
+                            job.exit_code = Some(code);
+
+                            self.progress.fail(job.clone()).await?;
+                        }
+                    },
+                    None => {
+                        job.exit_code = None;
+
+                        self.progress.terminate(job.clone()).await?;
+                    }
+                }
+
+                if self.queue.update(job.clone()).await.is_err() {
                     eprintln!("Failed to send output line");
                     break;
                 }
             }
         }
+        // let mut child = tokio::process::Command::new("sh")
+        //     .arg("-c")
+        //     .arg(&job.step.script.clone())
+        //     .stdout(Stdio::piped())
+        //     .stderr(Stdio::piped())
+        //     .spawn()?;
 
-        if let Some(stderr) = child.stderr.take() {
-            let mut lines = BufReader::new(stderr).lines();
-            while let Some(line) = lines.next_line().await? {
-                job.append_output(&line);
+        // if let Some(stdout) = child.stdout.take() {
+        //     let mut lines = BufReader::new(stdout).lines();
+        //     while let Some(line) = lines.next_line().await? {
+        //         job.append_output(&line);
 
-                if self.queue.update(*job.clone()).await.is_err() {
-                    eprintln!("Failed to send output line");
-                    break;
-                }
-            }
-        }
+        //         if self.queue.update(*job.clone()).await.is_err() {
+        //             eprintln!("Failed to send output line");
+        //             break;
+        //         }
+        //     }
+        // }
 
-        let output = child.wait_with_output().await?;
+        // if let Some(stderr) = child.stderr.take() {
+        //     let mut lines = BufReader::new(stderr).lines();
+        //     while let Some(line) = lines.next_line().await? {
+        //         job.append_output(&line);
+
+        //         if self.queue.update(*job.clone()).await.is_err() {
+        //             eprintln!("Failed to send output line");
+        //             break;
+        //         }
+        //     }
+        // }
+
+        // let output = child.wait_with_output().await?;
         job.finished_at = Some(Utc::now());
 
-        match output.status.code() {
-            Some(code) => match code {
-                0 => {
-                    job.exit_code = output.status.code();
-
-                    self.progress.success(*job.clone()).await?;
-                }
-                code => {
-                    job.exit_code = Some(code);
-
-                    self.progress.fail(*job.clone()).await?;
-                }
-            },
-            None => {
-                job.exit_code = None;
-
-                self.progress.terminate(*job.clone()).await?;
-            }
-        }
-
-        if self.queue.update(*job.clone()).await.is_err() {
+        if self.queue.update(job.clone()).await.is_err() {
             eprintln!("Failed to send output line");
         }
 
