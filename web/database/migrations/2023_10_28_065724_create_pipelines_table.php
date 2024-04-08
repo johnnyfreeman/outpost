629baa149695cae24b5c6fcdeb30f3576acb72eb
John Freeman <prsjohnny@gmail.com>
2024-04-07 23:25:46 -0400

    env and current directory

--- web/database/migrations/2023_10_28_065724_create_pipelines_table.php
+++ web/database/migrations/2023_10_28_065724_create_pipelines_table.php
@@ -31,7 +31,9 @@ return new class extends Migration {
             $table->ulid('id')->unique();
             $table->foreignUlid('pipeline_id')->references('id')->on('pipelines');
             $table->string('name');
-            $table->longText('script')->nullable();
+            $table->longText('env')->nullable();
+            $table->string('current_directory')->nullable();
+            $table->longText('script');
             $table->timestampsTz();
         });
 
