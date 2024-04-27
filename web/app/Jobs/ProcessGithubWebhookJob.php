1dd400062de2d629a88b29ff56c1fb907b4faa31
John Freeman <prsjohnny@gmail.com>
2024-04-27 11:21:35 -0400

    typing

--- web/app/Jobs/ProcessGithubWebhookJob.php
+++ web/app/Jobs/ProcessGithubWebhookJob.php
@@ -16,6 +16,7 @@ class ProcessGithubWebhookJob extends ProcessWebhookJob
 
         if ($headers->contains('x-github-event', 'push')) {
             GithubSetting::with('pipeline.steps')->where('trigger_on_push', true)->chunkById(500, function ($settings) use ($payload) {
+                /** @var \App\Models\GithubSetting */
                 foreach ($settings as $setting) {
                     $setting->pipeline->run($payload->get('head_commit.url'));
                 }
