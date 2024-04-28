79827dec8ab6c4036401a91cd502d0d9785e5b84
John Freeman <prsjohnny@gmail.com>
2024-04-28 12:50:50 -0400

    logging

--- web/app/Listeners/Github/CreateCommitStatus.php
+++ web/app/Listeners/Github/CreateCommitStatus.php
@@ -6,10 +6,10 @@ use App\Events\PipelineJobFinished;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 
 class CreateCommitStatus implements ShouldQueue
 {
-
     public function handle(PipelineJobFinished $event): void
     {
         $ref = data_get($event, 'job.event.ref');
@@ -17,11 +17,19 @@ class CreateCommitStatus implements ShouldQueue
         $pipelineName = data_get($event, 'job.event.pipeline.name');
         $pipelineStepName = data_get($event, 'job.step.name');
 
-        Http::post("https://api.github.com/repos/johnnyfreeman/bilbo/statuses/{$ref}", [
+        $response = Http::post("https://api.github.com/repos/johnnyfreeman/bilbo/statuses/{$ref}", [
             "state" => "success",
             "target_url" => route('pipeline.show', ['pipeline' => $pipelineId]),
             "description" => "The pipeline job finished!",
             "context" => "{$pipelineName} / {$pipelineStepName}",
+        ]);
+
+        Log::debug('Creating commit status', [
+            'ref' => $ref,
+            'pipelineId' => $pipelineId,
+            'pipelineName' => $pipelineName,
+            'pipelineStepName' => $pipelineStepName,
+            'response' => $response,
         ]);
     }
 }
